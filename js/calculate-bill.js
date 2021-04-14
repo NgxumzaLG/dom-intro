4ae7209bebfc9003467c65976b3c434d93ac4ee9
NgxumzaLG <ngxumzalusanda@gmail.com>
2021-04-14 10:48:12 +0200

    Calculate-bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,45 @@
 //get a reference to the calculate button
+var calculateBtnElement = document.querySelector(".calculateBtn");
 
 //get a reference to the billTotal element
+var billTotalElement = document.querySelector(".billTotal");
 
 //get a reference to the billString
+    var billStringElement = document.querySelector(".billString");
 
 //create the function that will be called when the calculate button is pressed
+function calculateBtnClicked(){
 //  * this function should read the string value entered - split it on a comma.
+var billString = billStringElement.value;
+var billItems = billString.split(",");
+var billTotal = 0;
 //  * loop over all the entries in the the resulting list
+for (var i=0;i<billItems.length;i++){
+    var billItem = billItems[i].trim();
+    if (billItem === "call"){
+        billTotal += 2.75;
+    }
+    else if (billItem === "sms"){
+        billTotal += 0.75;
+    }
+}
 //  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
+var roundedBillTotal = billTotal.toFixed(2);                                     
+    billTotalElement.innerHTML = roundedBillTotal;
 
-//link the function to a click event on the calculate button
+//  * once done looping over all the entries - display the total onto the screen in the billTotal element
+if (billTotal >= 30) {
+    billTotalElement.classList.add("danger");
+    billTotalElement.classList.remove("warning");
+}
+else if (billTotal >= 20) {
+    billTotalElement.classList.add("warning");
+    billTotalElement.classList.remove("danger");
+}
+else{
+    billTotalElement.classList.remove("warning");
+    billTotalElement.classList.remove('danger');
+}
+}
+// link the function to a click event on the calculate button
+calculateBtnElement.addEventListener('click', calculateBtnClicked);
